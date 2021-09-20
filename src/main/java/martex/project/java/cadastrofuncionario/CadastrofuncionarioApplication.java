ad4f63926c70f6ae9f30f9dfab1c79460f43e1ec
martex84 <martex849@gmail.com>
2021-09-20 19:30:57 -0300

    Initial Project

---
+++ src/main/java/martex/project/java/cadastrofuncionario/CadastrofuncionarioApplication.java
@@ -0,0 +1,13 @@
+package martex.project.java.cadastrofuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrofuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrofuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/martex/project/java/cadastrofuncionario/CadastrofuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package martex.project.java.cadastrofuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrofuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
